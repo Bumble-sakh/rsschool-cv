179d5163d7aba4aea9fe045bc764f68a1abff505
Bumble <bumble.sakh@gmail.com>
2021-12-30 03:07:41 +1100

    feat: add slider to expiriance

--- main.js
+++ main.js
@@ -66,3 +66,20 @@ let observer = new IntersectionObserver(
 )
 
 observer.observe(document.querySelector('.skills-container'))
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
